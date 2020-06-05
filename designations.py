e1146c84305ec26b67e0ca1f8c6b5cf15797b1ad
gilettejr <31991967+gilettejr@users.noreply.github.com>
2020-06-05 16:50:41 +0100

    Code for evaluating marginals written

--- designations.py
+++ designations.py
@@ -6,26 +6,172 @@ class ratio_utils(sat):
     def __init__(self,*args,**kwargs):
         super(ratio_utils,self).__init__(*args,**kwargs)
         
-    
-    
-        jhc=np.array([0,0,0,10])
-        hkc=np.array([0,0,0,8])
-        jkc=np.array([0,0,0,4])
-        jk4c=np.array([0,0,0,2])
-        trgb=([0,0,0,18])
+        def CM_to_FEH(CM):
+            FEH=-1.39 -0.47*np.log10(CM)
         
-        cuts=np.array([jhc,hkc,jkc,jk4c,trgb])
+            return(FEH)            
+        jhc=np.array([0.81,0.70,0.77,10])
+        hkc=np.array([0.50,0.46,0.57,8])
+        jkc=np.array([1.33,1.30,1.42,1.60])
+        jk4m=np.array([0.99,0.988,0.96,0.86])
+        trgb=([18,17.8,17.8,18])
+        
+        cuts=np.array([jhc,hkc,jkc,jk4m,trgb])
         cut_frame=[]
         for i in range(len(cuts)):
-            print(i)
+
             for j in range(len(cuts[i])):
                 if self.infilenames[j]==self.galaxy:
                     cut_frame.append(cuts[i][j])
                     break
         cut_frame=np.array(cut_frame)
         
-        jhc=cut_frame[0]
-        hkc=cut_frame[1]
-        jkc=cut_frame[2]
-        jk4c=cut_frame[3]
-        trgb=cut_frame[4]
+        self.jhc=cut_frame[0]
+        self.hkc=cut_frame[1]
+        self.jkc=cut_frame[2]
+        self.jk4m=cut_frame[3]
+        self.trgb=cut_frame[4]
+        self.CM_to_FEH=CM_to_FEH
+
+        data=self.data
+        hk=data.hmag-data.kmag
+        jh=data.jmag-data.hmag
+        jk=data.jmag-data.kmag
+        mdata=data.copy()
+        cdata=data.copy()
+
+
+        for i in data.index:
+            if data.kmag[i] > self.trgb or jk[i] < self.jk4m:
+                mdata.loc[i]=np.nan
+                cdata.loc[i]=np.nan
+            elif (hk[i] > self.hkc and jh[i] > self.jhc)  or jk[i] > self.jkc:
+                mdata.loc[i]=np.nan
+                
+            else:
+                cdata.loc[i]=np.nan
+        
+        self.cdata=cdata.dropna()
+        self.mdata=mdata.dropna()
+    
+    def define_marginals(self,jkmult=1,hkmult=1,jhmult=1):
+        
+        for j in range(len(self.infilenames)):
+            
+            if self.galaxy==self.infilenames[j]:
+                k=j
+                break
+            
+        
+        jkdata=self.data.copy()
+        hkdata=self.data.copy()
+
+        jherrs=[0,0,0,0]
+        hkerrs=[0,0,0,0]
+        jkerrs=[0,0,0,0]
+        
+        hk=self.data.hmag-self.data.kmag
+        jh=self.data.jmag-self.data.hmag
+        jk=self.data.jmag-self.data.kmag
+        
+        
+        
+        
+        jkmmdata=self.data.copy()
+        jkMdata=self.data.copy()
+        jkcmdata=self.data.copy()
+        jkCdata=self.data.copy()
+        
+        hkmmdata=self.data.copy()
+        hkMdata=self.data.copy()
+        hkcmdata=self.data.copy()
+        hkCdata=self.data.copy()
+        
+
+        
+        for i in jkdata.index:
+            if jkdata.kmag[i] > self.trgb or jk[i] < self.jk4m:
+                jkMdata.loc[i]=np.nan
+                jkmmdata.loc[i]=np.nan
+                jkcmdata.loc[i]=np.nan
+                jkCdata.loc[i]=np.nan
+                jkdata.loc[i]=np.nan
+                
+                hkMdata.loc[i]=np.nan
+                hkmmdata.loc[i]=np.nan
+                hkcmdata.loc[i]=np.nan
+                hkCdata.loc[i]=np.nan
+                hkdata.loc[i]=np.nan
+                
+                
+            elif jk[i]>self.jkc-jkerrs[k]:
+                jkMdata.loc[i]=np.nan
+        
+        jkMdata=jkMdata.dropna()
+        jkmmdata=jkmmdata.dropna()
+        jkcmdata=jkcmdata.dropna()
+        jkCdata=jkCdata.dropna()
+        jkdata=jkdata.dropna()
+        
+        hkMdata=jkMdata.dropna()
+        hkmmdata=jkmmdata.dropna()
+        hkcmdata=jkcmdata.dropna()
+        hkCdata=jkCdata.dropna()
+        hkdata=jkdata.dropna()
+        
+        for i in jkdata.index:
+            if jk[i]<self.jkc - jkerrs[k] or jk[i] > self.jkc:
+                jkmmdata.loc[i]=np.nan
+        jkmmdata=jkmmdata.dropna()
+        
+        for i in jkdata.index:
+            if jk[i] < self.jkc or jk[i] > self.jkc + jkerrs[k]:
+                jkcmdata.loc[i]=np.nan
+        jkcmdata=jkcmdata.dropna()
+        
+        for i in jkdata.index:
+            if jk[i] > self.jkc + jkerrs[k]:
+                jkCdata.loc[i].dropna()
+        jkCdata=jkCdata.dropna()
+        
+        
+        for i in hkdata.index:
+            
+            if hk[i] > self.hkc-hkerrs[k] and jh[i] > self.jhc-jherrs[k]:
+                hkMdata.loc[i]=np.nan
+        hkMdata=hkMdata.dropna()
+        
+        for i in hkdata.index:
+            
+            if (hk[i] > self.hkc and jh[i] > self.jhc) or (hk[i] < self.hkc-hkerrs[k] or jh[i] < self.jhc-jherrs[k]):
+                hkmmdata.loc[i]=np.nan
+                
+        hkmmdata=hkmmdata.dropna()
+        
+        for i in hkdata.index:
+            
+            if (hk[i] > self.hkc+ hkerrs[k] and jh[i] > self.jhc + jherrs[k]) or (hk[i]<self.hkc or jh[i] < self.jhc):
+                hkcmdata.loc[i]=np.nan
+        hkcmdata=hkcmdata.dropna()
+        
+        for i in hkdata.index:
+            
+            if hk[i] < self.hkc+hkerrs[k] or jh[i] > self.jhc+jherrs[k]:
+                hkCdata.loc[i]=np.nan
+        hkCdata=hkCdata.dropna()
+                
+
+
+
+            
+        
+        
+    
+    def total_CM_FEH(self):
+        
+        cm=np.divide(len(self.cdata),len(self.mdata))
+        FEH= self.CM_to_FEH(cm)
+        
+        print('Average C/M ratio = ' + str(cm) +', average [Fe/H] = ' + str(FEH))
+        
+        
--- sat_graphers.py
+++ sat_graphers.py
@@ -12,8 +12,22 @@ class sat(graphs):
             c=np.sqrt(a**2+b**2)
             
             return c
-        self.mag=quadrat
         
+        def data_select(data):
+            if data=='all':
+                outdata=self.data
+            elif data=='c':
+                outdata=self.cdata
+            elif data=='m':
+                outdata=self.mdata
+                
+            else:
+                print('Invalid dataset chosen in method')
+        
+            return(outdata)
+        
+        self.mag=quadrat
+        self.data_select=data_select
         infilenames=['ngc147','ngc185','ngc205','m32']
         
         for i in infilenames:
@@ -31,13 +45,35 @@ class sat(graphs):
                     break
                 pdata=pd.read_parquet(pinfile)
                 self.pdata=pdata
-        
+                
+                
+                
         self.galaxy=galaxy
-        self.data=data
+        self.data=data.dropna()
         self.infilenames=infilenames
-    def plot_tan(self,stars='all',marker='o',markersize=1,color='black'):
+    
+    def plot_kj_cmd(self,marker='o',markersize=1,color='blue',stars='all'):
         
-        data=self.data
+        data=self.data_select(stars)
+        plt.rc('axes',labelsize = 15)
+        plt.plot(data.jmag - data.kmag,data.kmag,linestyle='none',markersize=markersize,marker=marker,color=color)
+        plt.gca().invert_yaxis()
+        plt.ylabel('$K_0$')
+        plt.xlabel('$J_0$-$K_0$')    
+        
+    def plot_cc(self,marker='o',markersize=1,color='blue',stars='all'):
+        data=self.data_select(stars)
+        plt.rc('axes',labelsize = 15)
+        plt.plot(data.hmag - data.kmag,data.jmag-data.hmag,linestyle='none',markersize=markersize,marker=marker,color=color)
+        plt.ylabel('$J_0$-$H_0$')
+        plt.xlabel('$H_0$-$K_0$')
+        
+        
+    def plot_tan(self,marker='o',markersize=1,color='black',stars='all'):
+        
+        data=self.data_select(stars)
+        
+
         
         plt.rc('axes',labelsize = 15)
         plt.plot(data.xi,data.eta,linestyle='none',marker = marker,markersize=markersize,color=color)
